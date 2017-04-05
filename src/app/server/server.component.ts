6dd870032334f9879c2ef526f8a83caca0ea45a0
Michael Tran <michaelhuytran@gmail.com>
2017-04-05 07:18:47 -0700

    [bug] - ng serve

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by miketran on 4/4/17.
+ */
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-selector',
+    templateUrl: './server.component.html'
+})
+
+export class ServerComponent {
+
+}
